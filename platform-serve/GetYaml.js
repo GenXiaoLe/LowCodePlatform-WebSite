c25d120d1d260c5827fdc782fba197c50ce97030
梁亚根 <799383929@qq.com>
2022-01-13 19:43:15 +0800

    feat: 服务端文件

--- platform-serve/GetYaml.js
+++ platform-serve/GetYaml.js
@@ -1,12 +1,21 @@
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
+const yaml = require('js-yaml');
 
-const readFiles = (files) => {
+const readFiles = (fileName) => {
+  const PATH = '../platform-ui/src/yml';
+  const file = fs.readFileSync(`${PATH}/${fileName}.yml`, { encoding: 'utf8' })
+  const json = yaml.load(file)
 
+  console.log(json)
 }
 
-const getPath = (url) => {
-  return path.resolve(__dirname)
-}
+// const getPath = (fileName) => {
+//   const PATH = '../platform-ui/src/yml';
+//   return path.dirname(`${PATH}/${fileName}`);
+// }
 
-console.log(getPath())
+
+export {
+  readFiles
+}
